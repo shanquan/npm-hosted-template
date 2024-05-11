cee8323fee871da2411d07b126edeae6bc6c27df
wang.weili <wang.weili@byd.com>
2024-05-11 14:15:30 +0800

    chore: 新增env环境变量配置文件支持

--- vue.config.js
+++ vue.config.js
@@ -3,26 +3,6 @@
  * @since : 2020/11/25
  * @doc.proxy: https://github.com/chimurai/http-proxy-middleware
  */
-process.env.VUE_APP_DEV = "http://10.12.5.188:20003/"
-process.env.VUE_APP_LOCAL = "http://127.0.0.1:10020/"
-
-process.env.VUE_APP_CODE = "BYD_FLAMES"
-process.env.VUE_APP_PROJECT_ONLY = '{"id":3,"projectName":"Zatanna","projectCode":"zatanna"}' // 唯一项目，如果配置VUE_APP_PROJECT_ONLY，登录页不显示项目下拉框
-process.env.VUE_APP_ID = "com.byd.flames"
-process.env.VUE_APP_VERSION = "1.0.0"
-process.env.VUE_APP_BUILD = "Flames-M_builds_versionDate"
-
-process.env.VUE_APP_CUSTOMIZE = "CUSTOMIZED" // jenkins发布定制系统配置标识
-let config = {
-    title: "ADMIN",
-    Car_Trial: {
-        title: "管理系统"
-    }
-}
-if(config[process.env.VUE_APP_CUSTOMIZE]){
-    Object.assign(config,config[process.env.VUE_APP_CUSTOMIZE])
-    delete config[process.env.VUE_APP_CUSTOMIZE]
-}
 
 module.exports = {
     publicPath: "/admin", // /android_asset/www
@@ -35,14 +15,14 @@ module.exports = {
     // },
     pages: {
         index: {
-            title: config.title, // title
+            title: process.env.TITLE, // title
             entry: 'src/main.js'
         }
     },
     devServer: {
-        proxy: {
+        proxy: Object.assign({
             '/user/*': {
-                target: process.env.VUE_APP_DEV,
+                target: "http://10.12.5.188:20003/",
                 ws: true,
                 changeOrigin: true,
                 // proxyTimeout: 60000,
@@ -50,17 +30,6 @@ module.exports = {
                 //     console.log(proxyReq.getHeader('host')+proxyReq.path)
                 // }
             },
-            '/zatanna//*': {
-                target: process.env.VUE_APP_DEV,
-                changeOrigin: true
-            },
-            // '/zatanna//*': {
-            //     target: process.env.VUE_APP_LOCAL,
-            //     changeOrigin: true,
-            //     pathRewrite: {
-            //         "^/zatanna": '/'
-            //     }
-            // }
-        }
+        },JSON.parse(process.env.PROXY))
     }
 }
